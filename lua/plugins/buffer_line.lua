40b442a81a8af2f67edfa7c9b0f1fec6bafa784a
shabbir toha <shabbir.toha@gmail.com>
2026-03-09 11:39:31 +0600

    update buffer_line configs

--- lua/plugins/buffer_line.lua
+++ lua/plugins/buffer_line.lua
@@ -3,14 +3,27 @@ return {
 	enabled = true,
 	opts = {
 		options = {
+			-- Existing
 			numbers = "ordinal",
-			modified_icon = "",
 			color_icons = true,
-			separator_style = "thin", -- You can use: "slant", "thick", "thin", etc.
-			show_buffer_close_icons = true,
-			show_close_icon = false,
+			separator_style = "thick", -- "slant", "thick", "thin", etc.
 			diagnostics = "nvim_lsp",
 			diagnostics_update_in_insert = false,
+
+			-- Added
+			themable = true,
+
+			indicator = {
+				icon = "  ",
+				style = "icon",
+			},
+
+			show_buffer_close_icons = true,
+			show_close_icon = true, -- changed from false since you wanted it added
+
+			buffer_close_icon = "",
+			close_icon = "",
+			modified_icon = "",
 		},
 	},
 }
